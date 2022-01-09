57a8be4e7d353a98dd96868af35c8743ea9db984
perpel <572821520@qq.com>
2022-01-09 20:58:45 +0800

    test

--- language_test.go
+++ language_test.go
@@ -7,12 +7,21 @@ import (
 
 func TestInitLang(t *testing.T) {
 	handler := NewIniHandler("language", "en")
-	InitLang(handler)
+
+	demo := YamlDemo{}
+	handler2 := NewYamlHandler("language", "en", demo)
+
+	InitLang(handler, handler2)
 	section := GetSection("en", "system", "ini")
 	log.Println("section = ", section.(map[string]string))
 	value := GetSectionValue("en", "system", "a", "ini")
 	log.Println("value = ", value)
 	log.Println("value = ", GetSectionValue("en", "system", "bb", "ini"))
+
+	// **************************
+	log.Println(GetSection("en", "demo", "yaml"))
+	v := GetSectionValue("zh", "demo", "Age", "yaml")
+	log.Println(v.(int))
 }
 
 func TestYamlDemo_Path(t *testing.T) {
